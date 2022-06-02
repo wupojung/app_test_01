f0058067028e129a086f6cb2f1c77c2aca856a08
wupojung <wupojung@gmail.com>
2022-06-02 16:20:20 +0800

    新增 config 設定值

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config 
+{
+  public static string sheetId ="1FZi9sG2o9FGkJvyYo8nuhyCeVGfcRqNwUuSzUMddt0M";
+  public static string ranger="%E5%B7%A5%E4%BD%9C%E8%A1%A81!A2:E100";
+  public static string key = "AIzaSyAqD-STHxwk6_g30gv6k8-A6WsTZ2ig1dE";
+  
+}
